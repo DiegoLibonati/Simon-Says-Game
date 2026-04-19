f82261e772c0da0b7f2e883ef4293d670aa40afd
Die <dlibonati@leafnoise.io>
2026-04-18 22:57:11 -0300

    fix: clean jest.setup.ts

--- __tests__/jest.setup.ts
+++ __tests__/jest.setup.ts
@@ -1,12 +1,6 @@
 import "@testing-library/jest-dom";
 
-import { mockAssets } from "@tests/__mocks__/assets.mock";
 import { mockModes } from "@tests/__mocks__/modes.mock";
-
-jest.mock("@/assets/export", () => ({
-  __esModule: true,
-  default: mockAssets,
-}));
 
 jest.mock("@/constants/modes", () => ({
   __esModule: true,
